88595a55eebbeacf51838e81caff3e77226410bf
shubham-singla525 <manishsingla177@gmail.com>
2024-01-18 23:50:31 +0530

    added controller to add form data in model and then populate on page

--- src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.thymeleafdemo.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +21,19 @@ public class HelloWorldController {
     @GetMapping("/processForm")
     public String processForm(){
         return "processForm";
+    }
+
+    @GetMapping("/processFormVersionTwo")
+    public String processFormShoutOut(HttpServletRequest request, Model theModel){
+        // extract data from form
+        String name = request.getParameter("studentName");
+
+        // make some message
+        String result = "Yo! "+name;
+
+        // add in model
+        theModel.addAttribute("theName", name);
+        theModel.addAttribute("theMessage",result);
+        return "processFormShoutOut";
     }
 }
