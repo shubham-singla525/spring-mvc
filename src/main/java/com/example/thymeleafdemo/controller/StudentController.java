f09849dad53c8de06d2ee978c1a04b1ad36f91fb
shubham-singla525 <manishsingla177@gmail.com>
2024-01-23 23:23:57 +0530

    added code for how to create dynamic dropdown in form

--- src/main/java/com/example/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.thymeleafdemo.controller;
 
 import com.example.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +10,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 
         theModel.addAttribute("student",new Student("Sita","Ram"));
+
+        theModel.addAttribute("countries", countries);
 
         return "show-form";
     }
